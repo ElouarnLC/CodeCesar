d7c340cd3d267312a8b360f0f810f4a35249efa4
ElouarnLC <61230623+ElouarnLC@users.noreply.github.com>
2022-11-21 11:50:57 +0100

    Code Cesar v1

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿//int lettre = Convert.ToInt32('a');
+
+
+
+string ChiffrerCesar(char cle, string mot)
+{
+    char[] motCodeTable = new char[mot.Length];
+
+    int cle2 = cle - 97;
+
+    for (int i = 0; i < motCodeTable.Length; i++)
+    {
+        if (mot[i] != ' ')
+        {
+            if ((mot[i] + cle2) > 122) motCodeTable[i] = Convert.ToChar(96 + cle2 - (122 - mot[i]));
+            else motCodeTable[i] = Convert.ToChar(mot[i] + cle2);
+        }
+        else motCodeTable[i] = ' ';
+    }
+
+    return String.Join(null, motCodeTable);
+}
+
+string DechiffrerCesar(char cle, string mot)
+{
+    char[] motCodeTable = new char[mot.Length];
+
+    int cle2 = cle - 97;
+
+    for (int i = 0; i < motCodeTable.Length; i++)
+    {
+        if (mot[i] != ' ')
+        {
+            if ((mot[i] - cle2) < 97) motCodeTable[i] = Convert.ToChar(122 - cle2 + (mot[i] - 96));
+            else motCodeTable[i] = Convert.ToChar(mot[i] - cle2);
+        }
+        else motCodeTable[i] = ' ';
+    }
+
+    return String.Join(null, motCodeTable);
+}
+
+
+
+Console.Write("Clé (lettre entre a et z):");
+char cle = Convert.ToChar(Console.ReadLine()!);
+
+Console.Write("Mot à coder:");
+string mot = Console.ReadLine()!;
+
+Console.WriteLine(ChiffrerCesar(cle, mot));
+
+Console.WriteLine(DechiffrerCesar(cle, ChiffrerCesar(cle, mot)));
